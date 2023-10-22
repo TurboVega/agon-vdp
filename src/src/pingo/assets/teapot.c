b360a5bc2f9bde278d3d4b4b594f2baf68619391
Curtis Whitley <restday7@gmail.com>
2023-10-22 05:28:16 -0400

    remove temp code

--- src/src/pingo/assets/teapot.c
+++ src/src/pingo/assets/teapot.c
@@ -3474,11 +3474,3 @@ Mesh mesh_teapot = {
     .tex_indices = tex_indexes,
     .textCoord = &tex_coords
 };
-
-/*
-    int indexes_count;
-    uint16_t * pos_indices;
-    uint16_t * tex_indices;
-    Vec3f * positions;
-    Vec2f * textCoord;
-*/
